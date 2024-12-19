f549b5fbce18dc94884301bc1f33c5b6e80c0576
negativecrybaby <bluejulia489@gmail.com>
2024-12-19 16:59:49 -0600

    fix

--- dds.js
+++
@@ -1,6 +0,0 @@
-const dropDownButton = document.querySelector('.dropdown-button');
-const dropDownContent = document.querySelector('.dropdown-content');
-
-dropDownButton.addEventListener('click', function() {
-    dropDownContent.classList.toggle('show');
-});
